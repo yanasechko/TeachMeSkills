3a3f4a8e1959ec4c3150df6b4aedb9922faaf44c
yanasechko <89037361+yanasechko@users.noreply.github.com>
2021-08-17 10:09:20 +0300

    Add files via upload

---
+++ Lesson1/task6.java
@@ -0,0 +1,11 @@
+public class task6 {
+    public static void main(String[] args) {
+
+        int a = 9;
+        int b = 12;
+
+        if (a > b) {
+            System.out.println(a);
+        } else System.out.println(b);
+    }
+}
