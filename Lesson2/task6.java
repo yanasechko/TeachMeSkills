8f5255329fa6a062ad5941669d6c4ec6a3909d0b
yanasechko <89037361+yanasechko@users.noreply.github.com>
2021-08-18 18:39:51 +0300

    .

---
+++ Lesson2/task6.java
@@ -0,0 +1,13 @@
+package Lesson2;
+
+public class task6 {
+    public static void main(String[] args) {
+        int j = 2;
+        while (j <= 100) {
+            System.out.println(j);
+            j = j + 2;
+
+
+        }
+    }
+}
