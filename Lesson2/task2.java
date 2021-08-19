6f72ca3fc49e870bd6bd2a1fe52dbef42e9f500e
yanasechko <89037361+yanasechko@users.noreply.github.com>
2021-08-19 16:36:37 +0300

    .

---
+++ Lesson2/task2.java
@@ -0,0 +1,14 @@
+package Lesson2;
+
+public class task2 {
+    public static void main(String[] args) {
+
+        int a = 1;
+        for (int i = 0; i <= 24; i += 3) {
+
+            System.out.println(" Через " + i + " ч. будет " + +a + " амёб");
+            a *= 2;
+
+        }
+    }
+}
