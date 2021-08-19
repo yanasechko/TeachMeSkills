5a1a966de1362c0734b7ce7cd2fee2f67a16c7b8
yanasechko <89037361+yanasechko@users.noreply.github.com>
2021-08-19 16:41:06 +0300

    .

---
+++ Lesson2/task5.java
@@ -0,0 +1,16 @@
+package Lesson2;
+
+public class task5 {
+    public static void main(String[] args) {
+
+        double j = 2.54;
+        double sum = 0;
+
+        for (int i = 1; i <= 20; i++) {
+
+            sum = i * j;
+            System.out.println(i + " дюйм = " + sum + " см");
+
+        }
+    }
+}
